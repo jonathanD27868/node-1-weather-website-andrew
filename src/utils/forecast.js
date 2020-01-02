59e370265d41b2cfed6c0b70bbcdae7ae2887819
jonathan D <jonathan@macbook-pro-de-jonathan.home>
2020-01-02 17:53:44 +0100

    add max an low temperature of the day

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, cb) => {
             cb('Unable to find location', undefined);
         } else{
             
-            cb(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain.\nIcon to display: ${body.daily.data[0].icon}`);
+            cb(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out. The high today is ${body.daily.data[0].temperatureHigh}  with a low of ${body.daily.data[0].temperatureLow} There is a ${body.currently.precipProbability}% chance of rain.\nIcon to display: ${body.daily.data[0].icon}`);
         }
     });
 
